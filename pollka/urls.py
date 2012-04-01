6de95d7634587e9ce96d9247c72a4c3074df03f1
Sascha Peilicke <saschpe@gmx.de>
2012-04-01 20:41:39 +0200

    Some basic views like 'about' and 'contact'

--- pollka/urls.py
+++ pollka/urls.py
@@ -8,9 +8,12 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'pollka.views.index'),
-    #url(r'^accounts/', include('pollka.accounts.urls')),
+    url(r'^about/$', 'pollka.views.about'),
+    url(r'^accounts/', include('pollka.accounts.urls')),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^contact/$', 'pollka.views.contact'),
+    url(r'^imprint/$', 'pollka.views.imprint'),
     #url(r'^events/', include('pollka.events.urls')),
     url(r'^polls/', include('pollka.polls.urls')),
 )
--- pollka/views.py
+++ pollka/views.py
@@ -1,5 +1,41 @@
 # -*- coding: utf-8 -*-
 
+from django.core.mail import send_mail
+from django.shortcuts import render
+from pollka.forms import ContactForm
+
 
 def index(request):
-    pass
+    '''Render a fancy static frontpage.
+    '''
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def contact(request):
+    '''
+    '''
+    if request.method == 'POST': # If the form has been submitted...
+        form = ContactForm(request.POST) # A form bound to the POST data
+        if form.is_valid(): # All validation rules pass
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            sender = form.cleaned_data['sender']
+            cc_myself = form.cleaned_data['cc_myself']
+
+            recipients = ['info@pollka.com']
+            if cc_myself:
+                recipients.append(sender)
+
+            send_mail(subject, message, sender, recipients)
+            return HttpResponseRedirect('/thanks/') # Redirect after POST
+    else:
+        form = ContactForm() # An unbound form
+    return render(request, 'contact.html', {'form': form})
+
+
+def imprint(request):
+    return render(request, 'imprint.html')
